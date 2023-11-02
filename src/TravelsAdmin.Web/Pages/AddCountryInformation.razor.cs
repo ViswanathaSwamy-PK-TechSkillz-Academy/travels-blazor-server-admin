64869bdc1c238eee54aa4e0aa8a779a4b2d36179
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-02 09:04:53 +0530

    Swamy/31oct work (#1)
    * Initial Version
    
    * Initial Version
    
    * Update Travels.Admin.sln

---
+++ src/TravelsAdmin.Web/Pages/AddCountryInformation.razor.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Text.Json;
+using TravelsAdmin.Web.Data;
+
+namespace TravelsAdmin.Web.Pages;
+
+public partial class AddCountryInformation
+{
+
+    private AddCountryInformationDto addCountryInformationDto = new();
+    private bool isFormSubmitted = false;
+    private string? SuccessMessage { get; set; }
+    private string? ErrorMessage { get; set; }
+    private CountryInfoDto countryInfo = new();
+
+    public bool IsBusy { get; set; }
+
+    [Inject]
+    public IHttpClientFactory? _httpClientFactory { get; set; }
+
+    public AddCountryInformation()
+    {
+    }
+
+    private async Task HandleFormSubmit()
+    {
+        if (IsBusy)
+        {
+            return;
+        }
+
+        IsBusy = true;
+
+        try
+        {
+            var client = _httpClientFactory!.CreateClient("FlaskCountriesAPI");
+
+            var response = await client.GetAsync($"api/getcountryinfo?country_name={addCountryInformationDto.CountryName}");
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                var content = await response.Content.ReadAsStringAsync();
+                countryInfo = JsonSerializer.Deserialize<CountryInfoDto>(content);
+
+                //isFormSubmitted = true;
+                ErrorMessage = null;
+                StateHasChanged();
+            }
+            else
+            {
+                // Handle API error
+            }
+        }
+        catch (Exception ex)
+        {
+            SuccessMessage = null;
+            ErrorMessage = $"Error while adding country information: {ex.Message}";
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+}
+
+
+
+//var json = JsonSerializer.Serialize(new { country_name = addCountryInformationDto.CountryName });
+//var content = new StringContent(json, Encoding.UTF8, "application/json");
+//var response = await client.PostAsync("api/getcountryinfo", content);
---
+++ src/TravelsAdmin.Web/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using TravelsAdmin.Web.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Configure the Python Flask HttpClient for the Countries API
+builder.Services.AddHttpClient(builder.Configuration["CountriesApi:HttpClientName"]!, client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["CountriesApi:EndPointUrl"]!);
+    // Add any additional configurations for the API
+});
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
